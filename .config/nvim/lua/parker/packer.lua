2d0309345621118f71b2ca8ac6d7785069236c66
ParkerJones <34404606+ParkerJones@users.noreply.github.com>
2025-04-23 00:26:15 +0000

    Fixed/removed submodule

--- .config/nvim/lua/parker/packer.lua
+++ .config/nvim/lua/parker/packer.lua
@@ -8,5 +8,6 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use "rebelot/kanagawa.nvim"
+  use 'rebelot/kanagawa.nvim'
+
 end)
