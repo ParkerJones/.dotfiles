8aff4bd6ce8cdc58ed3e5233fbe39f9701deb493
ParkerJones <34404606+ParkerJones@users.noreply.github.com>
2025-06-10 19:34:04 +0000

    Added .zshrc

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ echo "This will remove existing dot files/directories and create new symlinks:"
 echo "~/.gitconfig"
 echo "~/.config/nvim"
 echo "~/.config/i3"
+echo "~/.zshrc"
 
 read -p "Are you sure? [y/n]: " confirm
 
@@ -17,11 +18,13 @@ if [[ "$confirm" =~ ^[Yy]$ ]]; then
 	[ -e ~/.gitconfig ] && rm -rf ~/.gitconfig
 	[ -e ~/.config/nvim ] && rm -rf ~/.config/nvim
 	[ -e ~/.config/i3 ] && rm -rf ~/.config/i3
+	[ -e ~/.zshrc ] && rm -rf ~/.zshrc
 
 	# Create symlinks
 	ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 	ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 	ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+	ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 	echo "Done!"
 else
